f6c4229e5a7bfdf4a39c3262fe1ed3e4a25ab7eb
vuducthinh <20021444@vnu.edu.vn>
2021-10-15 11:12:08 +0700

    Bai tap DSA

--- week5/SimpleTextEditor.java
+++ week5/SimpleTextEditor.java
@@ -4,52 +4,33 @@ import java.util.Scanner;
 
 public class SimpleTextEditor {
 
-    static class Editor {
-        Stack<String> stack = new Stack<>();
-        String s = "";
-
-        public void append(String w) {
-            s += w;
-            stack.push(w);
-        }
-
-        public void delete(int k) {
-            s = s.substring(0, s.length() - k);
-            stack.push(s);
-        }
-
-        public void print(int k) {
-            System.out.println(s.charAt(k-1));
-        }
-
-        public void undo() {
-            stack.pop();
-            s = stack.peek();
-        }
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        Editor text = new Editor();
+        Stack<String> stack = new Stack<>();
+        String s = "";
+        stack.push(s);
 
         for (int i = 0; i < n; i++) {
             int type = sc.nextInt();
             if (type == 1) {
                 String w = sc.next();
-                text.append(w);
+                s += w;
+                stack.push(s);
             }
             if(type == 2) {
                 int K = sc.nextInt();
-                text.delete(K);
+                s = s.substring(0, s.length() - K);
+                stack.push(s);
             }
             if(type == 3) {
                 int k = sc.nextInt();
-                text.print(k);
+                System.out.println(s.charAt(k-1));
             }
             if (type == 4) {
-                text.undo();
+                stack.pop();
+                s = stack.peek();
             }
         }
     }
